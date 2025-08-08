41069c7691dfde367835e6cada8dd395567984d0
simi216 <s.aulich@stud.uni-goettingen.de>
2025-08-08 16:57:15 +0200

    siamese model in testing

--- src/model/wrapper/Siamese.py
+++ src/model/wrapper/Siamese.py
@@ -2,7 +2,7 @@ import numpy as np
 import tensorflow as tf
 import keras
 
-def make_siamese_encoder(input_shape, base_model : keras.Model, num_contrastive_views=4):
+def make_siamese_encoder(base_model : keras.Model, num_contrastive_views=4):
     """
     Create a Siamese autoencoder model that applies the base model to multiple views of the input data.
     
@@ -14,11 +14,18 @@ def make_siamese_encoder(input_shape, base_model : keras.Model, num_contrastive_
     Returns:
         keras.Model: A new model that outputs predictions and optionally latent representations.
     """
-    views = [keras.layers.Input(shape=input_shape, name=f"view_{i}") for i in range(num_contrastive_views)]
+    input_shapes = [input_shape[1:] for input_shape in base_model.input_shape] # Get the input shape from the base model
+
+    # Create input layers for each view
+    views = [[keras.Input(shape=input_shape, name=f"view_{j}_input_{i}") for i, input_shape in enumerate(input_shapes)] for j in range(num_contrastive_views)]
+
+
     # Apply the base model to each view
     outputs = [base_model(view) for view in views]
     # Concatenate the latent spaces
     concatenated_latent = keras.layers.Concatenate(axis=-1, name="concatenated_latent")(outputs)
+
+    views = [inputs for view in views for inputs in view]  # Flatten the list of lists (e.g., [[a, b], [c, d]] -> [a, b, c, d])
 
     # Create the Siamese model
     siamese_model = keras.Model(inputs=views, outputs=concatenated_latent, name="SiamesePredictionModel")
